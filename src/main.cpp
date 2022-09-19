5e07a4002ffe2e6c568f132895b593ff3fc9ade0
Rawad <rawadhaddad1999@gmail>
2022-09-19 16:57:06 -0400

    Final Code

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,55 @@
 #include <Arduino.h>
 
-void setup() {
-  // put your setup code here, to run once:
+// I, Rawad Haddad (000777218), certify that this material is my original work. No other person's work has been used without due acknowledgement and I have not made my work available to anyone else.
+
+void setup()
+{
+  // configure the USB serial monitor
+  Serial.begin(115200);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop()
+{
+  int iVal;
+  float analogVal;
+  float temp;
+  String judge;
+
+  // read digitized value from the D1 Mini's A/D convertor
+  iVal = analogRead(A0);
+  analogVal = iVal * (3.3 / 1023.0);
+  temp = analogVal * (50 / 3.3);
+
+  if (temp < 10)
+  {
+    judge = "Cold!";
+  }
+  else if (temp <= 15)
+  {
+    judge = "Cool";
+  }
+  else if (temp <= 25)
+  {
+    judge = "Perfect";
+  }
+  else if (temp <= 30)
+  {
+    judge = "Warm";
+  }
+  else if (temp < 35)
+  {
+    judge = "Hot";
+  }
+  else if (temp > 35)
+  {
+    judge = "Too Hot!";
+  }
+
+  // print value to the USB port
+  // Serial.println("Digitized Value = " + String(iVal));
+  Serial.printf("Digitized Value = %s, which is equivalent to an Analog Voltage = %sV\n", String(iVal), String(analogVal));
+  Serial.printf("Temperature = %s deg. C which is %s\n", String(temp), judge);
+
+  // wait 2 seconds
+  delay(2000);
 }
